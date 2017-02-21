0d7468d7c9a0e4d8df1c0d86b8c0cd545a97adaa
tharikaGitHub <tharika.m92@gmail.com>
2017-02-21 17:40:01 +0530

    After code formatting and quality checking

---
+++ OrderServiceMediator/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+_
+/src/main/java/mediator/CustomClassMediator.java,7/b/7b0c2b83a11f743dd1f0ed69871aa61000dd3082
--- OrderServiceMediator/src/main/java/mediator/CustomClassMediator.java
+++ OrderServiceMediator/src/main/java/mediator/CustomClassMediator.java
@@ -1,3 +1,23 @@
+/*
+ *
+ *   Copyright (c) ${date}, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ * /
+ */
+
 package mediator;
 
 import org.apache.commons.logging.Log;
@@ -7,21 +27,23 @@ import org.apache.synapse.mediators.AbstractMediator;
 
 import java.util.Iterator;
 
-/**
- * Created by tharika on 2/20/17.
- */
-
 public class CustomClassMediator extends AbstractMediator {
 
     private static final Log logger = LogFactory.getLog(CustomClassMediator.class);
 
+    /**
+     * This is the method used for intercepting the message context and performing required operations.
+     *
+     * @param messageContext - message context
+     * @return - true after successful mediation
+     */
     public boolean mediate(MessageContext messageContext) {
 
         logger.info("Inside the mediate method");
 
         Iterator itr = messageContext.getEnvelope().getBody().getFirstElement().getChildElements();
 
-        while(itr.hasNext()) {
+        while (itr.hasNext()) {
             logger.info(itr.next());
         }
 
@@ -29,14 +51,17 @@ public class CustomClassMediator extends AbstractMediator {
         return true;
     }
 
+    @Override
     public String getType() {
         return null;
     }
 
+    @Override
     public void setTraceState(int traceState) {
         traceState = 0;
     }
 
+    @Override
     public int getTraceState() {
         return 0;
     }
